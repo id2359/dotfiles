54f5709e10f80a196991dfaa155d947a51efc067
Lee Render <id2359@yahoo.com>
2016-02-15 14:48:02 +0800

    added evil mode manually

--- emacs/emacs.d/myutils/mysettings.el
+++ emacs/emacs.d/myutils/mysettings.el
@@ -3,4 +3,36 @@
 
 (require 'color-theme)
 (color-theme-initialize)
-(load-file "color-theme-blackboard.el")
+(color-theme-calm-forest)
+
+(add-hook
+ 'after-init-hook
+ '(lambda ()
+    
+    ;; ido mode
+    (require 'ido)
+    (setq ido-enable-flex-matching t)
+    (setq ido-everywhere t)
+    (ido-mode t)
+
+    ;; autocomplete
+
+    (require 'auto-complete)
+
+    (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+    (require 'auto-complete-config)
+    (ac-config-default)
+    (global-auto-complete-mode t)
+
+    ;; projectile
+    (require 'projectile)
+    (projectile-global-mode)
+
+    ;; Jedi
+    (require 'jedi)
+
+    ;; neotree
+    (require 'neotree)
+    (global-set-key [f8] 'neotree-toggle)
+    ))
+
