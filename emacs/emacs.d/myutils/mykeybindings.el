cb6104c59554e02d8488740dfb5cb1440e5d3bdb
Lee Render <id2359@yahoo.com>
2016-02-19 16:06:43 +0800

    Easier buffer switching

--- emacs/emacs.d/myutils/mykeybindings.el
+++ emacs/emacs.d/myutils/mykeybindings.el
@@ -1,4 +1,8 @@
 (global-set-key "\C-cd" 'kill-whole-line)
 (global-set-key (kbd "C-S-<f3>") 'magit-status)
 (global-set-key (kbd "C-<f8>") 'pep8)
+
+(global-set-key (kbd "C-<left>") 'previous-buffer)
+(global-set-key (kbd "C-<right>") 'next-buffer)
+
 (provide 'mykeybindings)
