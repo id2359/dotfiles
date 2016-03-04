cb24eff9a99e27b136fef7a9ada3f163f62b864d
Lee Render <id2359@yahoo.com>
2016-03-04 15:40:36 +0800

    Added some utils to kill buffers

--- emacs/emacs.d/myutils/myutils.el
+++ emacs/emacs.d/myutils/myutils.el
@@ -6,13 +6,31 @@
 (defun my-search (term)
   (interactive "sSearch Term: ")
   (let
-      ((buf (get-buffer-create search-buffer)))
+      ((buf (get-buffer-create search-buffer))
+       (pyfiles "*.py")
+       (project-root "/home/lee/src/rdrf"))
     
   (set-buffer buf)
   (erase-buffer)
   (insert (concat "search for " term " will go here"))
+
+  (rgrep term pyfiles project-root)
   (display-buffer buf)))
 
+
+
+(defun kill-buffers ()
+  (interactive)
+  (mapcar 'kill-buffer (buffer-list)))
+
+
+
+(defun kill-other-buffers ()
+  (interactive)
+  (mapcar 'kill-buffer (delq  (current-buffer) (buffer-list))))
+
+	
+  
 
 
 
