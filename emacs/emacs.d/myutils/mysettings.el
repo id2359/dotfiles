dea111121b4989a7c9631ceaf13bb7056be87064
Lee Render <id2359@yahoo.com>
2016-05-12 09:37:06 +0800

    django mode

--- emacs/emacs.d/myutils/mysettings.el
+++ emacs/emacs.d/myutils/mysettings.el
@@ -55,6 +55,12 @@
     ;; flycheck
     (global-flycheck-mode)
 
+    (add-hook 'python-mode-hook
+      (lambda ()
+        (setq indent-tabs-mode nil)
+        (setq tab-width 4)
+        (setq python-indent 4)))
+
     ;; yasnippet
     (require 'yasnippet)
     (setq yas-snippet-dirs '("~/.emacs.d/snippets"))
